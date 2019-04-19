302d89189423d73b44ac5dabdc52ae730281df6f
Kurtis <kurtie@microsoft.com>
2019-04-18 17:34:28 -0700

    Uncomment

--- EditorMotionControllerDLL/MotionController.Shared/MotionController.cpp
+++ EditorMotionControllerDLL/MotionController.Shared/MotionController.cpp
@@ -28,61 +28,61 @@ EXTERN_C __declspec(dllexport) uint32_t TryGetMotionControllerModel()
 	return 1000;
 }
 
-//EXTERN_C __declspec(dllexport) BOOL TryGetMotionControllerModel(_In_ UINT32 controllerId, _Out_ UINT32& outputSize, _Outptr_result_bytebuffer_(outputSize) BYTE& outputBuffer)
-//{
-//    SpatialInteractionSource source = GetSpatialInteractionSource(controllerId);
-//
-//    if (source != nullptr)
-//    {
-//        // Start getting the renderable model stream asynchronously.
-//        IAsyncOperation<IRandomAccessStreamWithContentType> modelOperation;
-//        try
-//        {
-//            modelOperation = source.Controller.TryGetRenderableModelAsync();
-//        }
-//        catch (...)
-//        {
-//            return FALSE;
-//        }
-//
-//        IRandomAccessStreamWithContentType stream = modelOperation.get();
-//
-//        // If the model call failed or the resulting stream is empty, return.
-//        if (stream == nullptr || stream.Size == 0)
-//        {
-//            return FALSE;
-//        }
-//
-//        // Create a buffer from the stream, to read the contents into a byte array to pass back to the app.
-//        unsigned int streamSize = (unsigned int)stream.Size;
-//		IAsyncOperationWithProgress<IBuffer, uint32_t> readOperation = stream.ReadAsync(Buffer(streamSize), streamSize, InputStreamOptions::None);
-//
-//        IBuffer buffer = readOperation.get();
-//
-//		// If the model call failed or the resulting stream is empty, return.
-//		if (buffer == nullptr || buffer.Length == 0)
-//		{
-//			return FALSE;
-//		}
-//
-//        //if (status != task_status::completed)
-//        //{
-//        //    return FALSE;
-//        //}
-//
-//        outputSize = buffer.Length;
-//
-//        // Now, create a DataReader from the buffer, which can then transfer the bytes into a byte array.
-//        DataReader reader = DataReader::FromBuffer(buffer);
-//		std::array<BYTE, buffer.Length> test;
-//        outputBuffer = test;
-//		reader.ReadBytes({ outputBuffer });
-//
-//        return TRUE;
-//    }
-//
-//    return FALSE;
-//}
+EXTERN_C __declspec(dllexport) BOOL TryGetMotionControllerModel(_In_ UINT32 controllerId, _Out_ UINT32& outputSize, _Outptr_result_bytebuffer_(outputSize) BYTE& outputBuffer)
+{
+    SpatialInteractionSource source = GetSpatialInteractionSource(controllerId);
+
+    if (source != nullptr)
+    {
+        // Start getting the renderable model stream asynchronously.
+        IAsyncOperation<IRandomAccessStreamWithContentType> modelOperation;
+        try
+        {
+            modelOperation = source.Controller.TryGetRenderableModelAsync();
+        }
+        catch (...)
+        {
+            return FALSE;
+        }
+
+        IRandomAccessStreamWithContentType stream = modelOperation.get();
+
+        // If the model call failed or the resulting stream is empty, return.
+        if (stream == nullptr || stream.Size == 0)
+        {
+            return FALSE;
+        }
+
+        // Create a buffer from the stream, to read the contents into a byte array to pass back to the app.
+        unsigned int streamSize = (unsigned int)stream.Size;
+		IAsyncOperationWithProgress<IBuffer, uint32_t> readOperation = stream.ReadAsync(Buffer(streamSize), streamSize, InputStreamOptions::None);
+
+        IBuffer buffer = readOperation.get();
+
+		// If the model call failed or the resulting stream is empty, return.
+		if (buffer == nullptr || buffer.Length == 0)
+		{
+			return FALSE;
+		}
+
+        //if (status != task_status::completed)
+        //{
+        //    return FALSE;
+        //}
+
+        outputSize = buffer.Length;
+
+        // Now, create a DataReader from the buffer, which can then transfer the bytes into a byte array.
+        DataReader reader = DataReader::FromBuffer(buffer);
+		std::array<BYTE, buffer.Length> test;
+        outputBuffer = test;
+		reader.ReadBytes({ outputBuffer });
+
+        return TRUE;
+    }
+
+    return FALSE;
+}
 
 SpatialInteractionSource GetSpatialInteractionSource(UINT32 controllerId)
 {
